411531cda1d01911de9044b63a512fbb7b7538d9
Douglas de Freitas <douglasmfreitas@gmail.com>
2019-11-16 10:25:03 -0300

    Updates

---
+++ precache-manifest.1e820e20a2289a86d959b70e6d58188a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1a02fc435a7df326e3c82fdaa57fef0",
+    "url": "/colors/index.html"
+  },
+  {
+    "revision": "4996ab285ee2f3c55c7c",
+    "url": "/colors/static/css/main.1218c231.chunk.css"
+  },
+  {
+    "revision": "3c9e2e3bcee91b5195e9",
+    "url": "/colors/static/js/2.6f709f85.chunk.js"
+  },
+  {
+    "revision": "4996ab285ee2f3c55c7c",
+    "url": "/colors/static/js/main.37dbb867.chunk.js"
+  },
+  {
+    "revision": "57a745e5a45d7cf4e9c2",
+    "url": "/colors/static/js/runtime-main.7b85bbb8.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/colors/static/media/logo.25bf045c.svg"
+  }
+]);
